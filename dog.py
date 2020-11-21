598b3b9d2ce1dd026cbae5bbb416f86674ee690b
oconda90 <70242240+oconda90@users.noreply.github.com>
2020-11-21 14:03:38 -0800

    created 2 files

---
+++ dog.py
@@ -0,0 +1,14 @@
+
+# dog.py
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print("dog initialized!")
+
+    def bark(self):
+        print("Woof!")
+
+my_dog = Dog("Rex", "SuperDog")
+print(my_dog.breed)
+my_dog.bark()
